2b8252830114ea615f84a5bebab1214ad5aefff2
Lukas Fuchs <wz49igir@studserv.uni-leipzig.de>
2025-11-07 14:49:47 +0100

    2nd commit

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
 #this is a test file for git
-    print("Hello, this is a test file for git.")
+print("Hello, this is a test file for git.")
+
+#this is the change before 2nd commit
+print("This line was added before the 2nd commit.")
